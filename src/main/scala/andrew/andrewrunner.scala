ff8a5106f083f34c2f00c2d931ba8ec2485fc423
andrewjonca <andrewjonca@gmail.com>
2016-03-10 13:55:45 -0800

    adding HWData.scala

--- src/main/scala/andrew/andrewrunner.scala
+++ src/main/scala/andrew/andrewrunner.scala
@@ -2,4 +2,9 @@ package andrew
 
 object andrewrunner extends App {
   println("Glorious project started!")
+  val basicTypes = new BasicTypes
+  basicTypes.showVectorType
+
+  val data = HWData.load
+  println(s"data: $data")
 }
